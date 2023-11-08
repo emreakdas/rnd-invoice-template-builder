4633111b7717654f9c99c1cd94edfd080ec9b40c
Emre AKDAŞ <eemreakdas@gmail.com>
2023-11-09 00:33:22 +0300

    change playground

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,13 +1,10 @@
 import componentsData from "../data/components-data.json";
-import { Fragment, createRef, useState, useEffect } from "react";
-import { flushSync } from "react-dom";
-import Draggable from "react-draggable";
-import { Resizable } from "re-resizable";
+import { useEffect } from "react";
 import { useAppContext } from "../context";
+import { Rnd } from "react-rnd";
 
 function Playground({ windowSize }) {
   const { selectedComponents, setSelectedComponents } = useAppContext();
-  const [resizeNode, setResizeNode] = useState(false);
 
   useEffect(() => {
     function handleResize() {
@@ -24,14 +21,7 @@ function Playground({ windowSize }) {
     };
   }, []);
 
-  function handleOnResizeStart() {
-    flushSync(() => {
-      setResizeNode(true);
-    });
-  }
-
   function handleOnResizeStop(ref, key) {
-    setResizeNode(false);
     setSelectedComponents(
       selectedComponents.map((item) => {
         if (item.key === key) {
@@ -43,11 +33,12 @@ function Playground({ windowSize }) {
     );
   }
 
-  function handleOnStop(data, key) {
+  function handleOnDragStop(data, key) {
     setSelectedComponents(
       selectedComponents.map((item) => {
         if (item.key === key) {
-          (item.x = data.x), (item.y = data.y);
+          item.x = data.x;
+          item.y = data.y;
         }
 
         return item;
@@ -55,45 +46,28 @@ function Playground({ windowSize }) {
     );
   }
 
+  const boxStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  };
+
   return (
     <div id="playground-inside">
       {selectedComponents.map(({ key }) => {
-        const ref = createRef(null);
         return (
-          <Fragment key={key}>
-            <Draggable
-              disabled={resizeNode}
-              nodeRef={ref}
-              bounds="parent"
-              defaultPosition={{ x: 0, y: 0 }}
-              position={null}
-              scale={1}
-              onStop={(e, data) => handleOnStop(data, key)}
-            >
-              <div
-                ref={ref}
-                style={{
-                  position: "absolute",
-                  top: "0",
-                  left: "0",
-                  cursor: "pointer",
-                }}
-              >
-                <Resizable
-                  className="box"
-                  minWidth={100}
-                  minHeight={40}
-                  maxWidth={600}
-                  onResizeStart={handleOnResizeStart}
-                  onResizeStop={(e, direction, ref) =>
-                    handleOnResizeStop(ref, key)
-                  }
-                >
-                  {componentsData[key]}
-                </Resizable>
-              </div>
-            </Draggable>
-          </Fragment>
+          <Rnd
+            minWidth="80px"
+            minHeight="50px"
+            key={key}
+            style={boxStyle}
+            className="box"
+            bounds="parent"
+            onDragStop={(e, d) => handleOnDragStop(d, key)}
+            onResizeStop={(e, direction, ref) => handleOnResizeStop(ref, key)}
+          >
+            {componentsData[key]}
+          </Rnd>
         );
       })}
     </div>
